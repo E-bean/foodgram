43fe2dc3cfdd75bd4a1d719fcce93e43701a756f
Татьяна Мамаева <tanjaka06@gmail.com>
2022-09-09 00:05:19 +0300

    delete comments

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,7 +1,5 @@
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
-# SubscribeViewSet
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 router_v1 = DefaultRouter()
@@ -12,12 +10,5 @@ router_v1.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    # path('users/subscriptions/', SubscribeViewSet.as_view(
-    #     {'get': 'list'}), name='subscriptions'
-    # ),
-    # path("users/<int:pk>/subscribe/", SubscribeViewSet.as_view(
-    #     {'post': 'create', 'delete': 'destroy'}), name='subscribe'
-    # ),
-    # path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -2,8 +2,6 @@ import os
 
 from dotenv import load_dotenv
 
-# from api.serializers import UserSerializer
-
 
 load_dotenv()
 
@@ -62,20 +60,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
